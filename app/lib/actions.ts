a0af68a7b7c7e971c2e2c3eb34be02d4fd9a0ba4
mobarak <mobarak.ice.06@gmail.com>
2025-01-26 01:58:41 +0600

    error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -48,6 +48,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+    // throw new Error('Failed to Delete Invoice');
     const path = `/${id}`
     await deleteInvoiceById(path,);
     revalidatePath('/dashboard/invoices');
